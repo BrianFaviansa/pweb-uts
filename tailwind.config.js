2c8b84672cb7652381eb6dcf1bb8ee592050156b
BrianFaviansa <brianfpd31@gmail.com>
2024-04-22 19:53:21 +0700

    [feat] done responsive

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}",
+  ],
+  theme: {
+    fontFamily: {
+      "NotoSans-Regular": ['NotoSans-Regular.ttf'],
+    },
+    extend: {
+    },
+    plugins: []
+  },
+}
